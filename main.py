0aa8cb85c1f5d5a65a6da1c4f81dbcbe794f08d6
ADARSH G <adarshg98765@gmail.com>
2021-05-20 22:05:34 +0530

    done

--- main.py
+++ main.py
@@ -18,4 +18,8 @@ async def on_message(msg):
   if msg.content.startswith("$score"):
     for x in mydivs:
       await msg.channel.send(x.get_text())
+  if msg.content.startswith("$weather"):
+
+    resp = requests.get("http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/193826?apikey=2A7yNWDokHDYBNQuyRJVYvgrdeg58flk%20&language=en-us&details=true&metric=true")
+    await msg.channel.send(resp.json())
 client.run('ODQzNDg1NTMzNDM4MzQ1MjU2.YKEjHg.ZMKOETN9UbGrGVn3u7V7Ynssrgs')
