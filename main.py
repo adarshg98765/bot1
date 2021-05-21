9884dac07473b6907b6272a301c3c99200d63073
ADARSH G <adarshg98765@gmail.com>
2021-05-21 19:49:14 +0530

    dd

--- main.py
+++ main.py
@@ -23,12 +23,12 @@ async def on_message(msg):
     resp = requests.get("http://dataservice.accuweather.com/forecasts/v1/hourly/1hour/193826?apikey=2A7yNWDokHDYBNQuyRJVYvgrdeg58flk%20&language=en-us&details=true&metric=true")
     await msg.channel.send(resp.json()[0]['RainProbability'])
     await msg.channel.send(" - Rain probability")
-    await msg.channel.send(resp.content)
   if msg.content.startswith("$cowin"):
     url1 = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=300&date=21-05-2021"
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
     resp1 = requests.get(url1,headers=headers)
     data = resp1.json()['sessions']
+
     for x in data:
       await msg.channel.send(x)
     
