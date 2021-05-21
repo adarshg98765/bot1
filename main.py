d6f52b697fb13905a4407d623ad9b7e265435d37
ADARSH G <adarshg98765@gmail.com>
2021-05-21 22:06:09 +0530

    ww

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import time
 from bs4 import BeautifulSoup
+from discord import state
 import requests
 url = "https://www.cricbuzz.com/cricket-match/live-scores"
 req = requests.get(url)
@@ -27,12 +28,22 @@ async def on_message(msg):
   if msg.content.startswith("$cowin"):
     headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.76 Safari/537.36'}
     if len(msg.content) == 6:
-      url2 = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/17"
-      resp2 = requests.get(url2,headers=headers)
-      data2 = resp2.json()['districts']
-      await msg.channel.send("District codes are ")
-      await msg.channel.send(data2)
-      await msg.channel.send("enter in following format : '$cowin disctrict_code date_dd-mm-yyyy'")
+      await msg.channel.send("State codes are ")
+      url3 = "https://cdn-api.co-vin.in/api/v2/admin/location/states"
+      resp3 = requests.get(url3,headers=headers)
+      data3 = resp3.json()['states']
+      for states in data3:
+        await msg.channel.send(states)
+      await msg.channel.send("Enter your state code")
+      async def on_message(msg1):
+        sc = msg1.content
+        url2 = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/{}".format(sc)
+        resp2 = requests.get(url2,headers=headers)
+        data2 = resp2.json()['districts']
+        await msg.channel.send("district codes acording to state you selected ")
+        for d in data2:
+          await msg.channel.send(d)
+        await msg.channel.send("enter in following format : '$cowin disctrict_code date_dd-mm-yyyy'")
     dis = msg.content[7:10]
     date = msg.content[11:21]
     url1 = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id={}&date={}".format(dis,date)
